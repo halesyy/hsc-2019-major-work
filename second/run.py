182dfb88b56480ef44feea1690d4f35259bc24ab
Jack Hales <halesyy@gmail.com>
2019-02-13 16:45:50 +1100

    added the generator working well

--- second/run.py
+++ second/run.py
@@ -5,15 +5,24 @@ size = [1920, 1080]
 im = Image.open("op.jpg")
 draw = ImageDraw.Draw(im)
 
-nodeAmount = 3
+nodeAmount = 12
 nodeStorage = []
 
 #/ First, generate the nodes coords.
 for i in range(0, nodeAmount):
-    nodeStorage[i] = [random.randint(0, size[0]), random.randit(0, size[1])]
+    nodeStorage.append([random.randint(0, size[0]), random.randint(0, size[1])])
+
+#/ Draw: .line(xy, fill=None, width=0, joint=None)
+for i in range(0, nodeAmount):
     print(nodeStorage)
+    for b in range(0, nodeAmount):
+        if b != i: #Parent does not equal the second check, connect B to I
+            firstNode = nodeStorage[i]
+            secndNode = nodeStorage[b]
+            draw.line([(firstNode[0], firstNode[1]), (secndNode[0], secndNode[1])], "#ffffff", random.randint(0,10))
 
 
+im.save("op-af.jpg")
 
 # while True:
 #     rx1 = random.randint(0, size[0])
