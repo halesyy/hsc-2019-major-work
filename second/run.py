87dfe95c90b39dd06d160dcae316762286457695
Jack Hales <halesyy@gmail.com>
2019-02-13 17:25:48 +1100

    added padding changes and some image changes as well as making everything a function

--- second/run.py
+++ second/run.py
@@ -1,35 +1,47 @@
 from PIL import Image, ImageDraw
 import random, time
 
+#/ Function to setup a padding that can't be picked at for random choices
+def randomPadChoice(padding):
+    global size
+    #x
+    x = random.randint(0+padding, size[0]-padding)
+    y = random.randint(0+padding, size[1]-padding)
+    return [x, y]
+
 size = [1920, 1080]
 im = Image.open("op.jpg")
 draw = ImageDraw.Draw(im)
 
-nodeAmount = 12
-nodeStorage = []
+def background():
+    backgroundDensity = 300
+    #/ Creating a background
+    for i in range(0, backgroundDensity):
+        rx1 = random.randint(0, size[0])
+        ry1 = random.randint(0, size[1])
+        siz = random.randint(1,3)
+        c   = random.choice(["a", "b", "c", "d", "e", "f"])
+        draw.rectangle([(rx1, ry1), (rx1+siz, ry1+siz)], "#{0}{1}{2}{3}{4}{5}".format(c, c, c, c, c, c))
 
-#/ First, generate the nodes coords.
-for i in range(0, nodeAmount):
-    nodeStorage.append([random.randint(0, size[0]), random.randint(0, size[1])])
+def nodeConnect():
+    nodeAmount = 4
+    nodeStorage = []
 
-#/ Draw: .line(xy, fill=None, width=0, joint=None)
-for i in range(0, nodeAmount):
-    print(nodeStorage)
-    for b in range(0, nodeAmount):
-        if b != i: #Parent does not equal the second check, connect B to I
-            firstNode = nodeStorage[i]
-            secndNode = nodeStorage[b]
-            draw.line([(firstNode[0], firstNode[1]), (secndNode[0], secndNode[1])], "#ffffff", random.randint(0,10))
+    #/ Generate the nodes coords on the screen.
+    for i in range(0, nodeAmount):
+        nodeStorage.append(randomPadChoice(0))
 
+    #/ Draw: .line(xy, fill=None, width=0, joint=None)
+    for i in range(0, nodeAmount):
+        print(nodeStorage)
+        for b in range(0, nodeAmount):
+            if b != i and random.randint(0,10) > 5: #Parent does not equal the second check, connect B to I
+                firstNode = nodeStorage[i]
+                secndNode = nodeStorage[b]
+                draw.line([(firstNode[0], firstNode[1]), (secndNode[0], secndNode[1])], "#c21f1f", random.randint(1,5))
+
+background()
+nodeConnect()
+nodeConnect()
 
 im.save("op-af.jpg")
-
-# while True:
-#     rx1 = random.randint(0, size[0])
-#     ry1 = random.randint(0, size[1])
-#     rx2 = rx1+boxsize
-#     ry2 = ry1+boxsize
-#     draw.rectangle([(rx1, ry1), (rx2, ry2)], '#000000')
-#     time.sleep(0.5)
-#     im.save("this.jpg")
-# im.rotate(45).show()
