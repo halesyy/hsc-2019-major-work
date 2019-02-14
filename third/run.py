0368a520f705f2712a3b1590b1f86e734e3fcd11
Jack Hales <halesyy@gmail.com>
2019-02-14 21:48:36 +1100

    added random colours

--- third/run.py
+++ third/run.py
@@ -1,8 +1,12 @@
 from PIL import Image, ImageDraw
 import random, time
 
-
-
+def RandomColour():
+    c = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+    colour = "#"
+    for i in range(0,6):
+        colour = colour + random.choice(c)
+    return colour
 
 
 #// -------------------------------------
@@ -17,7 +21,7 @@ class BG:
             ry1 = random.randint(0, size[1])
             siz = random.randint(1,3)
             c   = random.choice(["a", "b", "c", "d", "e", "f"])
-            draw.rectangle([(rx1, ry1), (rx1+siz, ry1+siz)], "#{0}{1}{2}{3}{4}{5}".format(c, c, c, c, c, c))
+            draw.rectangle([(rx1, ry1), (rx1+siz, ry1+siz)], RandomColour())
 
 class Bodies:
     def RandomPaddedCoords(self, pixel_padding):
@@ -35,14 +39,15 @@ class Bodies:
         for i in range(0, nodeAmount):
             nodeStorage.append(self.RandomPaddedCoords(0))
 
-        #/ Draw: .line(xy, fill=None, width=0, joint=None)
+        #/ Draw: .line(xy, fill=None, width=0, joint=None), iterating each generated node and writing lines
+        #/ to the lines which are NOT part of the array previously.
         for i in range(0, nodeAmount):
             print(nodeStorage)
             for b in range(0, nodeAmount):
                 if b != i and random.randint(0,10) > 5: #Parent does not equal the second check, connect B to I
                     firstNode = nodeStorage[i]
                     secndNode = nodeStorage[b]
-                    draw.line([(firstNode[0], firstNode[1]), (secndNode[0], secndNode[1])], colour, random.randint(1,5))
+                    draw.line([(firstNode[0], firstNode[1]), (secndNode[0], secndNode[1])], colour, random.randint(4,7))
 
 #// Image setup
 size = [10000, 10000]
@@ -55,6 +60,7 @@ BG.stars(10000)
 
 #// Bodies
 Bodies = Bodies()
+Bodies.GenerateNodes("#c21f1f")
 Bodies.GenerateNodes("#c21f1f")
 Bodies.GenerateNodes("#c21f1f")
 
