c4f5a78754e7880534046b09144a6432241a5667
Jack Hales <halesyy@gmail.com>
2019-02-14 21:38:05 +1100

    Setup class-system, but not partioning

--- third/create.py
+++ third/create.py
@@ -1,20 +1,5 @@
 from PIL import Image
 
-size = [1920, 1080]
+size = [10000, 10000]
 im = Image.new("RGB", size, "#000000")
 im.save("op.jpg")
-
-# draw = ImageDraw.Draw(im)
-#
-# boxsize = 6
-#
-# while True:
-#     rx1 = random.randint(0, size[0])
-#     ry1 = random.randint(0, size[1])
-#     rx2 = rx1+boxsize
-#     ry2 = ry1+boxsize
-#     draw.rectangle([(rx1, ry1), (rx2, ry2)], '#000000')
-#     time.sleep(0.5)
-#     im.save("this.jpg")
-
-# im.rotate(45).show()
