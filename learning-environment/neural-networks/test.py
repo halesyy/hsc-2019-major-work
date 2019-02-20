cebecca0ad550c93511117e2ebe5dc7278295a96
Jack Hales <halesyy@gmail.com>
2019-02-20 21:40:10 +1100

    working on a flower problem, as well as installing jupyter and numpy in a conda environment

--- learning-environment/neural-networks/test.py
+++ learning-environment/neural-networks/test.py
@@ -1,1 +1,27 @@
 # Solving problems thrown at them
+# Manual vs learning with Neural Network / Machine Learning
+
+# Problem: a girl notes down the colour, length and width of her flowers,
+# all of her flowers have been recorded with the common red or blue,
+# but it seems that she has forgotten to write down the colour of her last
+# flower.
+
+template = ["colour", "length", "width"]
+
+dataset = [
+    ["red", 3, 1.5],
+    ["blue", 2, 1],
+    ["red", 4, 1.5],
+    ["blue", 3, 1],
+    ["red", 3.5, 0.5],
+    ["blue", 2, 0.5],
+    ["red", 5.5, 1],
+    ["blue", 1, 1],
+    ["?", 4.5, 1]
+]
+
+# Manual way 1: get the average value of widthXlength for red/blue, then
+# compare.
+# Manual way 2: graphing a scatter plot, with the lengthxwidth being x/y values,
+# with the red/blue colour going on the plot. Plot the mystery value,
+# and see where it resides amongst the other colours.
