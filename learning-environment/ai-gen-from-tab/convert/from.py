0e6046285208cfe1d5150548a17d21c34010b6e0
Jack Hales <halesyy@gmail.com>
2019-02-26 07:59:58 +1100

    ok, this is epic

--- learning-environment/ai-gen-from-tab/convert/from.py
+++ learning-environment/ai-gen-from-tab/convert/from.py
@@ -1,9 +1,10 @@
 mnot = """
- / ( & = : > ?
-E9 > = ? ; C @ E C C @
-1 / / * 4 3 3 4 3 3 4 4 4 1 6 9 9 9 : ; ; @ < : 9 ; 9 > ; < 9 6+ 1 / ( 4 . 1 / / / 3 4 4
-* + 1 ! 3 + ( * 3 * & & !
-A<4/*& A<4/*& A:41+ 941*! ><
+  G H H
+/ > J E F J H G E C C C ? > > < < ; ; 9 @ >
+H F D D F I I E H J J M
+M F H H F E F C @ C F E C E = B
+; 9 9 ; 9 1 9 9 ; 9 ; J J
+O I J L9 $ B 8 <9 8 > = < ? : 8 / f J C ; H E 8 F! >9 ;61( 60+ : :
 """
 
 conversionClassifier = "! $ % & ' ( ) * + . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ ] ^ _ ` a b c d e f g h i j k l m n o p q r s t u v w x y z { | } ~ ° ± ² ³ ´ µ ¶ · ¸ ¹ º » ¼ ½ ¾ ¿ À Á Â Ã Ä Å Æ Ç È É Ê Ë Ì Í Î Ï  Ð Ñ Ò Ó Ô Õ Ö × Ø Ù Ú Û Ü Ý Þ ß"
@@ -20,19 +21,19 @@ def verticalStep(v, e, B, G, D, A, E):
         # large if statement, unclean but I wanna finish this
         note = converter[note]
         if note - 0 < 5:
-            tE, tA, tD, tG, tB, te = str(note-0)+"-", "--", "--", "--", "--", "--"
+            tE, tA, tD, tG, tB, te = str(note-0)+"--", "---", "---", "---", "---", "---"
         elif note - 5 < 5:
-            tE, tA, tD, tG, tB, te = "--",str(note-5)+"-", "--", "--", "--", "--"
+            tE, tA, tD, tG, tB, te = "---",str(note-5)+"--", "---", "---", "---", "---"
         elif note - 10 < 5:
-            tE, tA, tD, tG, tB, te = "--", "--", str(note-10)+"-", "--", "--", "--"
+            tE, tA, tD, tG, tB, te = "---", "---", str(note-10)+"--", "---", "---", "---"
         elif note - 15 < 5:
-            tE, tA, tD, tG, tB, te = "--", "--", "--", str(note-15)+"-", "--", "--"
+            tE, tA, tD, tG, tB, te = "---", "---", "---", str(note-15)+"--", "---", "---"
         elif note - 20 < 5:
-            tE, tA, tD, tG, tB, te = "--", "--", "--", "--", str(note-20)+"-", "--"
+            tE, tA, tD, tG, tB, te = "---", "---", "---", "---", str(note-20)+"--", "---"
         elif note - 25 < 5:
-            tE, tA, tD, tG, tB, te = "--", "--", "--", "--", "--", str(note-25)+"-"
+            tE, tA, tD, tG, tB, te = "---", "---", "---", "---", "---", str(note-25)+"---"
         else:
-            tE, tA, tD, tG, tB, te = "--", "--", "--", "--", "--", str(note-25)
+            tE, tA, tD, tG, tB, te = "---", "---", "---", "---", "---", str(note-25)+"-"
 
         E += tE
         A += tA
