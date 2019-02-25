74eeea9a9cbfc6e5388121f0053f352d93306558
Jack Hales <halesyy@gmail.com>
2019-02-25 22:45:56 +1100

    currently developed safedump.txt, racking up all the tabs

--- learning-environment/ai-gen-from-tab/lstm-rnn/r.py
+++ learning-environment/ai-gen-from-tab/lstm-rnn/r.py
@@ -1,2 +1,2 @@
 import tensorflow as tf
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
--- learning-environment/ai-gen-from-tab/tab2safe.py
+++ learning-environment/ai-gen-from-tab/tab2safe.py
@@ -7,6 +7,7 @@ import itertools
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 import collections
+from multiprocessing import Process
 
 # takes in a popualr mono-space tab format and
 # steps to generate a E-base-friendly tablature
@@ -21,8 +22,15 @@ for conv, i in zip(conversionClassifier, range(0, len(conversionClassifier))):
     converter["{0}".format(conv)] = i
 backConverter = {v: k for k, v in converter.items()}
 
+
+
+
+
 def allEqual(l):
     return np.unique(l).shape[0]<=1
+
+
+
 
 def getTabFromUrl(url):
     request  = Request(url)
@@ -50,8 +58,26 @@ def getTabFromUrl(url):
     # print(hsplit)
     return hsplit
 
+
+
+
 def vertical(strings, at):
     return [strings[0][at], strings[1][at], strings[2][at], strings[3][at], strings[4][at], strings[5][at]]
+
+
+
+# simple function
+def rhtml(url):
+    request  = Request(url)
+    response = urlopen(request)
+    html = response.read()
+    response.close()
+    soup = BeautifulSoup(html, features="html.parser")
+    phtml = soup.prettify()
+    return phtml
+
+
+
 
 def horizontal(string, weight):
     finalNote = 0
@@ -59,9 +85,7 @@ def horizontal(string, weight):
     storedNotes = []
     firstStoreLocation = 0
     time = []
-
     for note, location in zip(string, range(len(string))):
-
         try:
             note = int(note)
             # SUCCESSFUL INTEGER
@@ -91,81 +115,84 @@ def horizontal(string, weight):
                     storedNotes = []
                     time.append([firstStoreLocation, finalNote])
             split = True
-
-    # print()
-    # print(time)
     return time
 
 
 
 
+def pipeSafeFromUrl(url):
+    tab_data = getTabFromUrl(url)
+    lines = tab_data.split("\n")
+
+    safe = "\n"
+    safe += "-"*30
+    safe += "\n"
+    for line, lineno in zip(lines, range(0, len(lines))):
+        #start of a new tabstep
+        if len(line) > 0 and line[0] == 'e' and line[1] == '|':
+            e, B, G, D, A, E  = lines[lineno], lines[lineno+1], lines[lineno+2], lines[lineno+3], lines[lineno+4], lines[lineno+5]
+            # Iterating over each vertical length in an array [0, 1, 2, 3, 4, 5]
+            # vert = vertical([e, B, G, D, A, E], 0)
+            # vert = vertical([e, B, G, D, A, E], i)
+            e, B, G, D, A, E = horizontal(e, 25), horizontal(B, 20), horizontal(G, 15), horizontal(D, 10), horizontal(A, 5), horizontal(E, 0)
+            bar = {}
+            for i in itertools.chain(*[e, B, G, D, A, E]):
+                time = i[0]
+                note = backConverter[i[1]]
+                # note = str(i[1])
+                if time in bar: # the time is already there, so add another note
+                    bar[int(time)].append(note)
+                else:
+                    bar[int(time)] = [note]
+            bar_asc = dict(sorted(bar.items(), key = lambda x:x[0]))
+            for k in bar_asc:
+                # print(k, bar_asc[k])
+                safe += ''.join(bar_asc[k])
+                safe += ' '
+            safe += "" if lineno == len(lines)-1 else "\n"
+    if len(lines) < 5: return ""
+    return safe
+
+def PARALLEL_pipeSafeFromUrl(link):
+    safef = open("safedump.txt", "a+")
+    safe  = pipeSafeFromUrl(link)
+    print("Finished {0}!\n".format(link))
+    safef.write(safe)
+
+def filterForArtistOrTab(urlset):
+    filteredUrls = []
+    for url in urlset:
+        spl = url.split("/")
+        # print(spl)
+        if len(spl) > 3 and spl[3] == 'tab':
+            filteredUrls.append(url)
+        elif len(spl) > 3 and spl[3] == 'artist':
+            filteredUrls.append(url)
+    return filteredUrls
 
 
-tabs = getTabFromUrl("https://tabs.ultimate-guitar.com/tab/megadeth/holy_wars_the_punishment_due_tabs_521861")
-t = open("tabs.txt", "w+")
-t.write(tabs)
-# tabs = open("tabs.txt", "r")
-# tabs = tabs.read()
+# Iterates through each set of links presented to the bare content,
+# and finds links inside of those links, creating a large array of links
+# So far, we've processed: Metallica, Iron Maiden, Queen, Bach (classical), Classical artiss,
+# 
+def ArtistLinks():
+    dsf = open("contentset.txt", "r+", encoding="utf8")
+    ds  = dsf.read()
+    dsf.truncate(0)
+    urlsFromDS = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', ds)
+    filteredUrls = filterForArtistOrTab(urlsFromDS)
+    return filteredUrls
 
+def DumpLinksToSafe(links):
+    ps = []
+    for link in links:
+        p = Process(target=PARALLEL_pipeSafeFromUrl, args=[link])
+        p.start()
+    for p in ps:
+        p.join()
 
-lines = tabs.split("\n")
+        # safe = PARALLEL_pipeSafeFromUrl(link)
 
-# First tab is from 0 -> 5
-# AFTER 5th iter, we're starting on tab 2 @
-# 7 -> 12
-# itno = 0
-# converter, safe int
-safe = "\n\n"
-safe += "-"*30
-safe += "\n"
-for line, lineno in zip(lines, range(0, len(lines))):
-    #start of a new tabstep
-    if len(line) > 0 and line[0] == 'e' and line[1] == '|':
-        e, B, G, D, A, E  = lines[lineno], lines[lineno+1], lines[lineno+2], lines[lineno+3], lines[lineno+4], lines[lineno+5]
-        # Iterating over each vertical length in an array [0, 1, 2, 3, 4, 5]
-        # vert = vertical([e, B, G, D, A, E], 0)
-        # vert = vertical([e, B, G, D, A, E], i)
-        e, B, G, D, A, E = horizontal(e, 25), horizontal(B, 20), horizontal(G, 15), horizontal(D, 10), horizontal(A, 5), horizontal(E, 0)
-        bar = {}
-        for i in itertools.chain(*[e, B, G, D, A, E]):
-            time = i[0]
-            note = backConverter[i[1]]
-            # note = str(i[1])
-            if time in bar: # the time is already there, so add another note
-                bar[int(time)].append(note)
-            else:
-                bar[int(time)] = [note]
-
-        # bar = collections.OrderedDict(sorted(bar.items()))
-        # dict(sorted(bar.items()))
-        bar_asc = dict(sorted(bar.items(), key = lambda x:x[0]))
-        # print(bar_asc)
-
-        for k in bar_asc:
-            # print(k, bar_asc[k])
-            safe += ''.join(bar_asc[k])
-            safe += ' '
-        safe += "" if lineno == len(lines)-1 else "\n"
-
-# safe += "-"*30
-# safe += "\n\n"
-print(safe)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# indvs = tabs.split("\n")
-# print(indvs)
+if __name__ == '__main__':
+    ALINKS = ArtistLinks()
+    DumpLinksToSafe(ALINKS)
