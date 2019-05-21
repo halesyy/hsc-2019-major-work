5b514f11ce727422116e0d0e3a75b8fb715435bf
Jack Hales <jack.hales@192-168-1-105.tpgi.com.au>
2019-05-21 18:18:32 +1000

    Splitting grouper tests, not working

--- controller/break/ImageSplitter.py
+++ controller/break/ImageSplitter.py
@@ -63,38 +63,32 @@ class iSplitter:
             return False
         # return True
 
+
+
+
+
+
     def GroupStart(self):
-        # 1. iterating over
-        # meta-array  in  charge  for  random choices,  basically  when  doing the
-        # color iteration, cache that xy data into a 2D array that can be randomly
-        # chosen  then once it's used  in the  itertions, convert  the inside  X/Y
-        # into the index using the  X*Ywid+X 2d->1d  indexing algorithm and delete
-        # it so that the cache is removed
-        # iterate  by  the  meta  array's length  and as it goes  down  it's  good
-        # basically we're creating an abstraction layer that is abstractly
-        # interfering and copying data to group it by the colour and locationtype.
-        # the advantage is that we can simply start the grouping process from randomly
-        # choosing from one as the parent controller.
         xr, yr = random.choice(self.LocationCache)
-        # xr, yr = 0, 0
         self.CurrentGroup = 0
         self.Groups.append([]) #store
         toExpand = [[xr, yr]]
         pixel = self.Cache[yr][xr]
         r,g,b = pixel
 
-        top_diff    = 1.2 # 20% over
-        bottom_diff = 2 - top_diff # 20% under
+        top_diff    = 1.20            # 20% over
+        bottom_diff = 2.00 - top_diff # 20% under
         lastAm = len(self.LocationCache)
         sameAmountFor = 0
 
         while len(self.LocationCache) != 0:
-            if lastAm == 4:
-                lastAm = 0
+            print("g... {0}{1}".format(sameAmountFor, lastAm))
+            if sameAmountFor == 3:
+                lastAm, sameAmountFor = 0, 0
                 xr, yr = random.choice(self.LocationCache)
                 self.CurrentGroup += 1
-                print("Changed group...")
-                
+                print("Changed group from while...")
+
             for expand in toExpand:
 
                 x, y = expand
@@ -114,6 +108,7 @@ class iSplitter:
                 if len(self.LocationCache) == lastAm:
                     sameAmountFor += 1
                 else: sameAmountFor = 0
+                lastAm = len(self.LocationCache)
 
                 for move, to in Movements.items():
                     xset, yset = to
@@ -134,7 +129,17 @@ class iSplitter:
                     ): continue
                     else: toExpand.append([xset, yset])
 
-                lastAm = len(self.LocationCache)
+
+
+
+
+
+
+
+
+
+
+
 
     def GroupFrom(self, x, y):
         currentgroup = self.CurrentGroup
